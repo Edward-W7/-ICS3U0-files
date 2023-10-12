fc3befafc846d27dcf7449d636720fadb6658c05
Edward Wang <115112114+Edward-W7@users.noreply.github.com>
2023-10-12 13:57:30 -0400

    Create js05.js

---
+++ JavaScript/js05.js
@@ -0,0 +1,38 @@
+//This is required to get input from the user in the console.
+const readline = require('readline-sync');
+
+/*
+  Name:        Built in math functions
+  Purpose:     Familiarize myself with math functions in js
+
+  Author:      Edward Wang
+  Created:     11-Oct-2023
+  Updated:     11-Oct-2023
+*/
+
+//require('./lesson.js');
+
+//Code goes here
+
+//Take input from user
+n = parseInt(readline.question("How many numbers are you inputting? \n"));
+sum = 0;
+
+//Initialize max and min variables as the min and max possible value. respectively
+min = Number.MAX_SAFE_INTEGER;
+max = Number.MIN_SAFE_INTEGER;
+
+
+//Iterate through each input and update values accordingly
+for (i = 0; i < n; i++) {
+	num = parseFloat(readline.question(""));
+	sum += num;
+	min = Math.min(min, num);
+	max = Math.max(max, num);
+}
+
+//Output values
+console.log("The sum of the numbers is: " + sum);
+console.log("The average of the numbers is: " + Math.round(sum / n));
+console.log("The maximum of the numbers is: " + max);
+console.log("The minimum of the numbers is: " + min);
